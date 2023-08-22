de1bfab8f1035d3deee5493a54d0149b24f486ec
Rupesh <112386480+Rupesh180902@users.noreply.github.com>
2023-08-22 21:26:37 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import dice from "./dice.png";
+import divider from "./divider.png";
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [adviceNo, setAdviceNo] = useState("");
+  async function getAdvice() {
+    const data = await fetch("https://api.adviceslip.com/advice");
+    const res = await data.json();
+    setAdvice(res.slip.advice);
+    setAdviceNo(res.slip.id);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="advice-generator">
+      {/* <h1>Random Advice Generator</h1>
+      <div className="advice-container">
+        {advice && <p className="advice">{advice}</p>}
+        <button onClick={getAdvice}>Get Advice</button>
+      </div> */}
+
+      <div className="advice-generator-output">
+        <span className="advice-generator-advice-number">
+          ADVICE #{adviceNo}
+        </span>
+        <q className="advice-generator-quote">{advice}</q>
+
+        <picture className="advice-generator-divider">
+          <img src={divider} alt="div" style={{ width: "12rem" }} />
+        </picture>
+      </div>
+      <button onClick={getAdvice} className="advice-generator-btn">
+        <img
+          src={dice}
+          style={{ width: "4rem" }}
+          alt="dice img"
+          className="advice-generator-btn-img"
+        />
+      </button>
     </div>
   );
 }
